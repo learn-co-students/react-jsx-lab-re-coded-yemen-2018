1bb4bcc7ab76e54a049f0eb3904c315f8955a39f
sondos alansi <sondos.alansi@yahoo.com>
2018-10-23 18:13:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
 import Webpage from './components/Webpage';
+import FillerText from './components/FillerText';
 
 ReactDOM.render(
   (
     <div>
       <RegistrationForm />
+      <FillerText />
       <Webpage />
     </div>
   ),
